9a48a3bc8737f9d344f461abcce2730ee5680758
d.nekrasov <d.nekrasov@cian.ru>
2020-10-16 19:31:24 +0300

    add postgres

---
+++ IntegrationTestingSandbox/DataAccess/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using IntegrationTestingSandbox.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary1
+{
+    public class DataAccess : IDataAccess
+    {
+        private readonly TestDbContext _dbContext;
+
+        public DataAccess(TestDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<string> Get(CancellationToken cancellationToken)
+        {
+            return  _dbContext.Strings.FirstOrDefault()?.String;
+        }
+    }
+
+    public interface IDataAccess
+    {
+        Task<string> Get(CancellationToken cancellationToken);
+    }
+}
